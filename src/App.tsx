34dca9e2bfa9ce0dce520acdc7c313fa7b938dca
Artur <artur.h.0427@gmail.com>
2023-03-15 22:57:21 +0100

    Add editor switching system to `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import "./styles.css";
 /* Components */
 import Navbar from "./molecules/Navbar";
 import Project from "./molecules/Project";
+import Editor from "./scenes/Editor";
 
 /* Interfaces */
 interface Props {}
-interface State {}
+interface State {
+	editor_open: boolean
+}
 
 /* Main */
 export default class App extends React.PureComponent<Props, State> {
@@ -19,17 +22,29 @@ export default class App extends React.PureComponent<Props, State> {
 		super(props);
 
 		/* State */
-		this.state = {};
+		this.state = {
+			editor_open: false
+		};
+
+		/* Function bindings */
+		this.openEditor = this.openEditor.bind(this);
 	}
 
 	/* Lifecycle */
 	componentDidMount(): void {}
 	componentWillUnmount(): void {}
 
+	/* Functions */
+	openEditor(id: string) {
+		this.setState({ editor_open: true });
+	}
+
 	/* Render */
 	render() {
-		return (
-			<div className="column">
+		return (<>{
+			this.state.editor_open
+			? <Editor />
+			: <div className="column">
 
 				{/* [HEADERS] Navbar for enabling window-dragging */}
 				<Navbar />
@@ -40,15 +55,16 @@ export default class App extends React.PureComponent<Props, State> {
 						<h1># My Quicknotes</h1>
 					</div>
 
+					{/* Links to projects */}
 					<div className="project-list">
-						<Project />
-						<Project />
-						<Project />
-						<Project />
-						<Project />
+						<Project onOpen={this.openEditor} />
+						<Project onOpen={this.openEditor} />
+						<Project onOpen={this.openEditor} />
+						<Project onOpen={this.openEditor} />
+						<Project onOpen={this.openEditor} />
 					</div>
 				</main>
 			</div>
-		);
+		}</>);
 	};
 }
